10c0a4e756c822a92beffff3fa3f443ee56785c5
Eyal Liebermann <eyal.liebermann@gmail.com>
2015-11-08 18:27:01 +0000

    changing game passing to show view

--- app.rb
+++ app.rb
@@ -38,9 +38,10 @@ class HangpersonApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    letter = params && params[:guess] && params[:guess].to_s[0]
+    letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    redirect '/show'
+    @game.guess letter
+    redirect '/show' , :game => @game
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
